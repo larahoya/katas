9c532e7965baf2ce847cb001856985f83360ccea
lara.hoya <lara.hoya@cabify.com>
2022-11-17 09:09:20 +0100

    rename execute

--- mars_rover/Models/Rover.py
+++ mars_rover/Models/Rover.py
@@ -11,26 +11,27 @@ class Direction(str, Enum):
 
 
 class MarsRover:
-    def __init__(self, point: Point, direction: Direction):
+    def __init__(self, point: Point, direction: Direction, grid: Grid):
         self.point = point
         self.direction = direction
+        self.grid = grid
 
-    def move(self, grid: Grid, command: str) -> (Point, Direction):
+    def execute(self, command: str) -> (Point, Direction):
         wrap_point, wrap_direction = Point(self.point.x, self.point.y), Direction(self.direction)
         for i in command:
             match self.direction, i:
                 case Direction.north, "M":
-                    if grid.isobstacle(self.point.x, self.point.y + 1):
+                    if self.grid.isobstacle(self.point.x, self.point.y + 1):
                         return self.point, self.direction
                     self.point.y += 1
-                    if self.point.y >= grid.size:
+                    if self.point.y >= self.grid.size:
                         return wrap_point, wrap_direction
                 case Direction.north, "L":
                     self.direction = Direction.west
                 case Direction.north, "R":
                     self.direction = Direction.east
                 case Direction.south, "M":
-                    if grid.isobstacle(self.point.x, self.point.y - 1):
+                    if self.grid.isobstacle(self.point.x, self.point.y - 1):
                         return self.point, self.direction
                     self.point.y -= 1
                     if self.point.y < 0:
@@ -40,17 +41,17 @@ class MarsRover:
                 case Direction.south, "R":
                     self.direction = Direction.west
                 case Direction.east, "M":
-                    if grid.isobstacle(self.point.x + 1, self.point.y):
+                    if self.grid.isobstacle(self.point.x + 1, self.point.y):
                         return self.point, self.direction
                     self.point.x += 1
-                    if self.point.x >= grid.size:
+                    if self.point.x >= self.grid.size:
                         return wrap_point, wrap_direction
                 case Direction.east, "L":
                     self.direction = Direction.north
                 case Direction.east, "R":
                     self.direction = Direction.south
                 case Direction.west, "M":
-                    if grid.isobstacle(self.point.x - 1, self.point.y):
+                    if self.grid.isobstacle(self.point.x - 1, self.point.y):
                         return self.point, self.direction
                     self.point.x -= 1
                     if self.point.x < 0:
--- mars_rover/Tests/Examples_tests.py
+++ mars_rover/Tests/Examples_tests.py
@@ -6,28 +6,25 @@ from Models.Grid import *
 class TestExamples(unittest.TestCase):
 
     def test_base(self):
-        grid = Grid(10, [])
-        rover = MarsRover(Point(0, 0), Direction.north)
+        rover = MarsRover(Point(0, 0), Direction.north, Grid(10, []))
         command = "MMRMMLM"
-        result_point, result_direction = rover.move(grid, command)
+        result_point, result_direction = rover.execute(command)
         expected_point, expected_direction = Point(2, 3), Direction.north
         self.assertEqual(result_point, expected_point)
         self.assertEqual(result_direction, expected_direction)
 
     def test_wrap_around(self):
-        grid = Grid(10, [])
-        rover = MarsRover(Point(0, 0), Direction.north)
+        rover = MarsRover(Point(0, 0), Direction.north, Grid(10, []))
         command = "MMMMMMMMMM"
-        result_point, result_direction = rover.move(grid, command)
+        result_point, result_direction = rover.execute(command)
         expected_point, expected_direction = Point(0, 0), Direction.north
         self.assertEqual(result_point, expected_point)
         self.assertEqual(result_direction, expected_direction)
 
     def test_obstacle(self):
-        grid = Grid(10, [Point(0, 3)])
-        rover = MarsRover(Point(0, 0), Direction.north)
+        rover = MarsRover(Point(0, 0), Direction.north, Grid(10, [Point(0, 3)]))
         command = "MMMM"
-        result_point, result_direction = rover.move(grid, command)
+        result_point, result_direction = rover.execute(command)
         expected_point, expected_direction = Point(0, 2), Direction.north
         self.assertEqual(result_point, expected_point)
         self.assertEqual(result_direction, expected_direction)
